e03c90ee0c8a9be535a3d9ac75ecae5f17518d67
mineeagle <141904026+Mineeagle@users.noreply.github.com>
2023-08-18 14:13:49 +0200

    Add sending functionality
    Added a simple sending functionality

--- sender_class.py
+++ sender_class.py
@@ -6,20 +6,57 @@ class Sender:
     EMAIL_COL = "i"
     INVOICE_NUMBER_COL = "j"
     FEE_COL = "k"
+
+    CHECK_COL = "l"
+    CHECK_TRUE = "✔"
+    CHECK_FALSE = "X"
+
     START_ROW = 2
 
-    def __init__(self, excel_url, sender_email, sender_password):
+    def __init__(self, excel_url, sender_email, sender_password, year=2023):
+        self.year = year
+
         self.eh = ExcelHandler(excel_url)
-        self.es = EmailSender(sender_email, sender_password)
+        print("Excel file has been loaded.")
+        self.sender_email = sender_email
+        self.sender_password = sender_password
 
         self.start_sending()
 
     def start_sending(self):
+        # Handle excel extraction and add check col
         excel_information = self.get_information_from_excel()
+        print("Data from excel extracted.")
+        self.eh.write(f"{Sender.CHECK_COL}1", "E-Mail sent")
+        print("Added 'E-Mail sent' column.")
+
+        print("---")
+
+        # Start Handling
+        for row, element in enumerate(excel_information):
+            row += 2
+
+            # Handle if no email exists
+            if element[Sender.EMAIL_COL] is None:
+                self.eh.write(f"{Sender.CHECK_COL}{row}", Sender.CHECK_FALSE)
+                print(f"Mr./Mrs. {element[Sender.LAST_NAME_COL]} has no email address.")
+                continue
+
+            # Send email if email exists
+            email_sender = EmailSender(self.sender_email, self.sender_password)
+            body = self.get_email_body(
+                element[Sender.LAST_NAME_COL],
+                element[Sender.INVOICE_NUMBER_COL],
+                element[Sender.FEE_COL],
+                self.year,
+            )
+            subject = self.get_email_subject(element[Sender.INVOICE_NUMBER_COL])
+            email_sender.send_mail(body, subject, element[Sender.EMAIL_COL])
+            self.eh.write(f"{Sender.CHECK_COL}{row}", Sender.CHECK_TRUE)
+            print(f"Sent email to: {element[Sender.EMAIL_COL]}")
 
     def get_email_body(self, last_name, invoice_number, fee, year):
-        email_body = f"""
-Sehr geehrte(r) Frau/Herr {last_name},
+        email_body = f"""Sehr geehrte(r) Frau/Herr {last_name},
 
 uns ist aufgefallen, dass Sie Ihren Mitgliedsbeitrag für die DEB/DEJ für das Jahr {year} noch nicht gezahlt haben.
 Aus diesem Grund bitten wir Sie den Mitgliedsbeitrag von {fee}€ unter der Rechnungsnummer {invoice_number} auf folgendes Konto zu überweisen:
@@ -55,3 +92,4 @@ Deutscher Esperanto-Bund/Deutsche Esperanto-Jugend
                 }
             )
         return res
+
