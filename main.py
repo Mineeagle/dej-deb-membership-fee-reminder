f4e3188f160b2679b6ad16a8a5b3f0f7121733e8
mineeagle <141904026+Mineeagle@users.noreply.github.com>
2023-08-18 15:34:52 +0200

    Add main.py
    Added main.py which allows to use the tool via the CLI.

---
+++ main.py
@@ -0,0 +1,37 @@
+from sender_class import Sender
+from getpass import getpass
+
+
+def get_input():
+    url = input("Please enter the url of the Excel file. > ")
+    sender_email = input("Please enter the sender email adress. > ")
+    sender_password = getpass(
+        "Please enter the password for the entered email address. > "
+    )
+    year = input("Please enter the year for the invoice. > ")
+
+    smtp_server = input(
+        "Please enter the smtp_webserver. Standard is 'webmail.esperanto.de'. Press 'enter' to use this standard value. > "
+    )
+    if smtp_server == "":
+        smtp_server = "webmail.esperanto.de"
+
+    smtp_port = input(
+        "Please enter the smtp port. Standard is 587. Press enter to use this standard value. > "
+    )
+    if smtp_port == "":
+        smtp_port = 587
+    else:
+        smtp_port = int(smtp_port)
+
+    return url, sender_email, sender_password, year, smtp_server, smtp_port
+
+
+def main():
+    url, sender_email, sender_password, year, smtp_server, smtp_port = get_input()
+    print("---Got input---")
+    Sender(url, sender_email, sender_password, year, smtp_server, smtp_port)
+
+
+if __name__ == "__main__":
+    main()
